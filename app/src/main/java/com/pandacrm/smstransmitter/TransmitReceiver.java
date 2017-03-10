1c67db2ae9b4a647070dc0b3c9a750c691bee4de
PandaXu <371093864>
2017-03-10 11:23:41 +0800

    支持长短信的发送

--- app/src/main/java/com/pandacrm/smstransmitter/TransmitReceiver.java
+++ app/src/main/java/com/pandacrm/smstransmitter/TransmitReceiver.java
@@ -1,5 +1,6 @@
 package com.pandacrm.smstransmitter;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,8 +8,10 @@ import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -18,7 +21,11 @@ public class TransmitReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Bundle bundle = intent.getExtras();
+        String message = "";
         SmsMessage msg = null;
+
+        String transmitNunmber = MainActivity.getSettingNote(context,"number");
+
         if (null != bundle) {
             Object[] smsObj = (Object[]) bundle.get("pdus");
             for (Object object : smsObj) {
@@ -27,26 +34,32 @@ public class TransmitReceiver extends BroadcastReceiver {
                 SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 String receiveTime = format.format(date);
                 String number = msg.getOriginatingAddress();
-                String message = msg.getDisplayMessageBody();
-                message = "短信助手自动转发\n来自："+number+"\n时间："+receiveTime+"\n内容："+message;
+
+                if("".equals(message)){
+                    message = "短信助手自动转发\n来自："+number+"\n时间："+receiveTime+"\n内容：";
+                }
+
+                message =message+ msg.getDisplayMessageBody();
+            }
+
+            if (!transmitNunmber.equals("")){//第一次安装软件时，在没有设置转发号码的时候不转发
+                transmitMessageTo(context,transmitNunmber, message);
 
                 Log.i(TAG,message);
-                String transmitNunmber = MainActivity.getSettingNote(context,"number");
-                if (transmitNunmber.equals("")){//第一次安装软件时，在没有设置转发号码的时候不转发
-
-                }else {//添加了号码
-                    transmitMessageTo(transmitNunmber, message);
-                }
+                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
             }
         }
     }
 
-    public void transmitMessageTo(String phoneNumber,String message){//转发短信
+    public void transmitMessageTo(Context context,String phoneNumber,String message){//转发短信
         SmsManager manager = SmsManager.getDefault();
         /** 切分短信，每七十个汉字切一个，短信长度限制不足七十就只有一个：返回的是字符串的List集合*/
-        List<String> texts =manager.divideMessage(message);//这个必须有
-        for(String text:texts){
-            manager.sendTextMessage(phoneNumber, null, text, null, null);
+        if(message.length()>70){
+            ArrayList<String> texts =manager.divideMessage(message);//这个必须有
+            manager.sendMultipartTextMessage(phoneNumber,null,texts,null,null);
+        }else{
+            PendingIntent sentIntent = PendingIntent.getBroadcast(context, 0, new Intent(), 0);
+            manager.sendTextMessage(phoneNumber, null, message, sentIntent, null);
         }
     }
 }
